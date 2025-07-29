89deaf09170f4a8476d9173f11020134a0e77c26
ABHIN C <84432979+ABHINC03@users.noreply.github.com>
2025-07-29 22:04:02 +0530

    Time: 39 ms (58.82%) | Memory: 17.8 MB (36.92%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        
+        
+
+        if x<0:
+            x=-1*x
+            x=str(x)
+            l1=list(x)
+            l1.reverse()
+            x=''.join(l1)
+            x=int(x)
+            x=-1*x
+           
+        else:
+            x=str(x)
+            l1=list(x)
+            l1.reverse()
+            x=''.join(l1)
+            x=int(x)
+            
+        INT_MIN = -2**31        # -2147483648
+        INT_MAX = 2**31 - 1     # 2147483647
+        if x< INT_MIN or x > INT_MAX:
+            return 0
+        return x
+        
+            
+
+
