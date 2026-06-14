3ffde94407ec2c39b5016b20cc0ac8243540f659
ABHIN C <84432979+ABHINC03@users.noreply.github.com>
2026-06-14 21:54:34 +0530

    Time: 2 ms (17.74%) | Memory: 19.6 MB (7.54%) - LeetSync

---
+++ 342-power-of-four/power-of-four.py
@@ -0,0 +1,9 @@
+import math
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<1:
+            return False
+
+        x=math.log(n,4)
+        return x.is_integer()
+        
