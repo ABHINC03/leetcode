b98e0b40af67509b161010b340fbfb32e6fedf2a
ABHIN C <84432979+ABHINC03@users.noreply.github.com>
2026-06-18 15:46:05 +0530

    Time: 5 ms (36.74%) | Memory: 19.4 MB (23.87%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -2,8 +2,7 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         l=0
         r=x
-        if x==0 or x==1:
-            return x
+      
         while(l<=r):
             mid=int((l+r)/2)
             if mid*mid==x:
