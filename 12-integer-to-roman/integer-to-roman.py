6739f191e836b753b311288fdb40a693267da384
ABHIN C <84432979+ABHINC03@users.noreply.github.com>
2026-06-23 19:09:14 +0530

    Time: 6 ms (44.68%) | Memory: 19.4 MB (6.58%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,67 +1,40 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        nums = str(num)
 
         hashmap={1:'I',4:'IV',5:'V',6:'VI',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:"CD",500:"D",900:'CM',1000:"M"}
-        
+        nums = str(num)
         value=[]
         pownum=[int(number)*10**(len(nums)-i-1) for i,number in enumerate(nums)]
         for nums in pownum:
             if nums>0:
                 if (nums%1000)==0:
-                    if nums in hashmap:
-                        value.append(hashmap[nums])
-                    
-                    else:
-                        temp=nums
-                        while(temp>0):
-                            value.append(hashmap[1000])
-                            temp=temp-1000
-                        
+
+                    steps=1000
+                
                 elif (nums%100)==0:
 
-                    
-                
-                    temp=nums
-                    temps=[]
-                    while(temp>0):
-                        if temp in hashmap:
-                            temps.append(hashmap[temp])
-                            temp=temp-temp
-                        else:
-                            temps.append(hashmap[100])
-                            temp-=100
-                    temps.reverse()
-                    print(temps)        
-                    for values in temps:
-                        value.append(values)
+                    steps=100
+        
                 elif (nums%10)==0:
-                    temp=nums
-                    temps=[]
-                    while(temp>0):
-                        if temp in hashmap:
-                            temps.append(hashmap[temp])
-                            temp=temp-temp
-                        else:
-                            temps.append(hashmap[10])
-                            temp-=10
-                    temps.reverse()
-                    for values in temps:
-                        value.append(values)
+
+                    steps=10
+                
                 else:
 
-                    temp=nums
-                    temps=[]
-                    while(temp>0):
-                        if temp in hashmap:
-                            temps.append(hashmap[temp])
-                            temp=temp-temp
-                        else:
-                            temps.append(hashmap[1])
-                            temp-=1
-                    temps.reverse()
-                    for values in temps:
+                    steps=1
+                temp=nums
+                temps=[]
+                while(temp>0):
+                    if temp in hashmap:
 
-                        value.append(values)
+                        temps.append(hashmap[temp])
+                        temp=temp-temp
+                    else:
+
+
+                        temps.append(hashmap[steps])
+                        temp-=steps
+                temps.reverse()
+                for values in temps:
+                    value.append(values)
         return ''.join(value)
-            
